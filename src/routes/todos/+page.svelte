b71b914f1e785d6afe94411744c1fe454b99db3d
akm <akm2000@gmail.com>
2023-09-10 09:54:20 +0900

    :sparkles: Add form to src/routes/todos

--- src/routes/todos/+page.svelte
+++ src/routes/todos/+page.svelte
@@ -5,6 +5,16 @@
 <div class="centered">
 	<h1>todos</h1>
 
+	<form method="POST">
+		<label>
+			add a todo:
+			<input
+				name="description"
+				autocomplete="off"
+			/>
+		</label>
+	</form>
+
 	<ul class="todos">
 		{#each data.todos as todo (todo.id)}
 			<li>

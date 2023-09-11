7ce97ceeef9ffb8a3961a9f87282fa812f348b5f
akm <akm2000@gmail.com>
2023-09-11 20:58:16 +0900

    :sparkles: Use json function to make response

--- src/routes/api/rolls/+server.ts
+++ src/routes/api/rolls/+server.ts
@@ -1,9 +1,6 @@
+import { json } from '@sveltejs/kit';
+
 export function GET() {
 	const number = Math.floor(Math.random() * 6) + 1;
-
-	return new Response(number.toString(), {
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	});
+	return json(number);
 }

2fe14dec9b7e4edfac6c9dfd117d7ea471681057
akm <akm2000@gmail.com>
2023-09-16 18:39:21 +0900

    :sparkles: Set aria-current by using $page.url.pathname

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
+<script type="ts">
+	import { page } from '$app/stores';
+</script>
+
 <nav>
-	<a href="/">home</a>
+	<a href="/" aria-current={$page.url.pathname === '/'}>home</a>
 	<a href="/blog">blog</a>
 	<a href="/todos">TODOs</a>
 	<a href="/todos2">TODOs2</a>
 	<a href="/roll">role</a>
 	<a href="/a/deeply/nested/route">a deeply nested route</a>
-	<a href="/about">about</a>
+	<a href="/about" aria-current={$page.url.pathname === '/about'}>about</a>
 </nav>
 
 <slot />

a2f50cd104ab6f344adea6ec405f127822196ee5
akm <akm2000@gmail.com>
2023-09-18 10:49:40 +0900

    :sparkles: Add type annotations about handle arguments and return value

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,10 @@
-export async function handle({ event, resolve }) {
-	console.log('handle is called');
-	return await resolve(event);
+import { RequestEvent, MaybePromise, ResolveOptions } from '@sveltejs/kit';
+
+// https://kit.svelte.jp/docs/types#public-types-handle
+export async function handle(arg: {
+	event: RequestEvent;
+	resolve(event: RequestEvent, opts?: ResolveOptions): MaybePromise<Response>;
+}): MaybePromise<Response> {
+	await console.log('handle is called');
+	return await arg.resolve(arg.event);
 }

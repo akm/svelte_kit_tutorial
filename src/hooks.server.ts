07c2c4313a88844849df4753311eae43633b7211
akm <akm2000@gmail.com>
2023-09-18 17:56:03 +0900

    :sparkles: define handleFetch to intercept /handle_fetch/a

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,7 +4,7 @@ import { RequestEvent, MaybePromise, ResolveOptions } from '@sveltejs/kit';
 export async function handle(arg: {
 	event: RequestEvent;
 	resolve(event: RequestEvent, opts?: ResolveOptions): MaybePromise<Response>;
-}): MaybePromise<Response> {
+}): Promise<Response> {
 	const { event, resolve } = arg;
 	await console.log('handle is called');
 	if (event.url.pathname === '/ping') {
@@ -12,4 +12,18 @@ export async function handle(arg: {
 	}
 	event.locals = { answer: 42 };
 	return await resolve(event);
+}
+
+// https://kit.svelte.jp/docs/types#public-types-handle
+export async function handleFetch(arg: {
+	event: RequestEvent;
+	request: Request;
+	fetch: typeof fetch;
+}): Promise<Response> {
+	const { request, fetch } = arg;
+	const url = new URL(request.url);
+	if (url.pathname === '/handle_fetch/a') {
+		return await fetch('/handle_fetch/b');
+	}
+	return await fetch(request);
 }
